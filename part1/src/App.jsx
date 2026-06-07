344df63bc0b62c9b7a554829b494419ba1e9d6a0
devhouda <houdadjemai.1@gmail.com>
2026-06-07 19:48:23 +0100

    react debugging

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,16 @@
 import { useState } from "react";
 
-const Display = ({ counter }) => <div>{counter}</div>;
-
-const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
-
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [value, setValue] = useState(10);
 
-  console.log("rendering with counter value", counter);
-
-  const increaseByOne = () => {
-    console.log("increasing, value before", counter);
-    setCounter(counter + 1);
-  };
-
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", counter);
-    setCounter(counter - 1);
-  };
-
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter);
-    setCounter(0);
+  const handleClick = () => {
+    setValue(0);
   };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {value}
+      <button onClick={handleClick}>reset to zero</button>
     </div>
   );
 };
