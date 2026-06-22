40620a922b5414744d94d624dd4442263cb2ba50
devhouda <houdadjemai.1@gmail.com>
2026-06-22 11:10:56 +0100

    Limiting creating new notes to logged-in users

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const middleware = require("./utils/middleware");
 const notesRouter = require("./controllers/notes");
 const usersRouter = require("./controllers/users");
 const dns = require("node:dns/promises");
+const loginRouter = require("./controllers/login");
 
 dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
@@ -28,6 +29,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
